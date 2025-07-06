7f549d706a73cdafd74106e1b91bc55a1eaf52e8
yann SIMON <yann.simon@teads.tv>
2025-07-06 20:13:30 +0200

    test email

--- test/EmailService.spec.ts
+++ test/EmailService.spec.ts
@@ -1,18 +1,58 @@
-import {describe, it, expect, jest} from '@jest/globals'
+import {describe, it, expect, jest, afterEach} from '@jest/globals'
 import EmailService from '../src/EmailService'
 import UserService from '../src/UserService'
 
 describe('EmailService', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
   describe('#send', () => {
     it('Should send an email if user exists', async () => {
+      //fixtures
+      jest.useFakeTimers();
+      const log = jest.spyOn(console, "log").mockImplementation(() => {});
+      const userService = {
+        getUserEmail: jest.fn().mockReturnValue("pierre@gmail.com"),
+      } as any as jest.Mocked<UserService>;
+
+      const emailService = new EmailService(userService);
+
+      //test
+      // call the method to create the timeout before advancing the timer
+      const sent = emailService.send("pierre","body")
+      // advance timer to avoid faked network delay
+      jest.advanceTimersByTime(10001);
+      // wait for response after advancing the time
+      await sent;
+      expect(log).toHaveBeenCalledWith('Email sent to pierre@gmail.com with body: body')
     })
     it('should not send an email if user does not exist', async () => {
+      //fixtures
+      jest.useFakeTimers();
+      const log = jest.spyOn(console, "log").mockImplementation(() => {});
+      const userService = {
+        getUserEmail: jest.fn().mockReturnValue(null),
+      } as any as jest.Mocked<UserService>;
+
+      const emailService = new EmailService(userService);
+
+      //test
+      try {
+        // call the method to create the timeout before advancing the timer
+        const sent = emailService.send("pierre","body")
+        // advance timer to avoid faked network delay
+        jest.advanceTimersByTime(10001);
+        // wait for response after advancing the time
+        await sent;
+      } catch (e) {}
+      expect(log).not.toHaveBeenCalled();
+
     })
     it('should return a failure if user does not exist', async () => {
       //fixtures
       jest.useFakeTimers();
       const userService = {
-        send: jest.fn().mockReturnValue(null),
+        getUserEmail: jest.fn().mockReturnValue(null),
       } as any as jest.Mocked<UserService>;
 
       const emailService = new EmailService(userService);
@@ -20,15 +60,18 @@ describe('EmailService', () => {
       //test
       let hasFailed = false;
       try {
+        // call the method to create the timeout before advancing the timer
         const sent = emailService.send("userId","body")
+        // advance timer to avoid faked network delay
         jest.advanceTimersByTime(10001);
+        // wait for response after advancing the time
         await sent;
       } catch (e) {
+        //assertion
         hasFailed = true;
         expect(e).toBeDefined()
       }
       expect(hasFailed).toBeTruthy()
-      
     })
   })
 })
