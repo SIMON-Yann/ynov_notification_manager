b4bea9951c0bbf92c97e5c584703dc9e160240f5
yann SIMON <yann.simon@teads.tv>
2025-07-06 19:16:21 +0200

    add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,20 @@
-const { createDefaultPreset } = require("ts-jest");
+const {createDefaultPreset} = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+    testEnvironment: "node",
+    transform: {
+        ...tsJestTransformCfg,
+    },
     testMatch: ["<rootDir>/test/**/*.spec.ts"],
+    coverageThreshold: {
+        global: {
+            branches: 50,
+            functions: 50,
+            lines: 50,
+            statements: 50,
+        },
+    },
 };
